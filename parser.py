2f451b5da1c8fc0845ad8e39aeb5558b13b98621
Kevin Bray <brayk@oregonstate.edu>
2018-10-07 16:22:55 -0700

    Parse command into array of strings [verb, obj, prep]

--- parser.py
+++ parser.py
@@ -1,8 +1,10 @@
-input = ['go north', 'take candle', 'drop key', 'get the candle key and map']
+input = ['go north', 'take candle', 'drop key',
+         'get the candle key and map', 'put the map in the chest', 'get on the map']
 
-verbs = ['go', 'take', 'drop', 'get']
-objects = ['candle', 'key', 'map']
+verbs = ['go', 'take', 'drop', 'get', 'put']
+objects = ['candle', 'key', 'map', 'chest']
 directions = ['north', 'south', 'east', 'west']
+prepositions = ['in', 'on']
 
 
 def find_verb(input):
@@ -14,11 +16,13 @@ def find_verb(input):
             # print ('    %d: found %s in verbs' % (num_found, word))
             verb = word
     if num_found == 1:
-        print ('    Verb: %s' % verb)
+        return (0, verb)
     elif num_found == 0:
-        print '    Verb: VERB NOT FOUND'
+        # print '    Verb: VERB NOT FOUND'
+        return (-1, 'ERR: verb not found')
     elif num_found > 1:
-        print '    Verb: TOO MANY VERBS FOUND'
+        # print '    Verb: TOO MANY VERBS FOUND'
+        return (-2, 'ERR: too many verbs')
 
 
 def find_object(input):
@@ -29,11 +33,11 @@ def find_object(input):
             num_found += 1
             obj = word
     if num_found == 1:
-        print ('    Object: %s' % obj)
+        return (0, obj)
     elif num_found == 0:
-        print '    Object: OBJECT NOT FOUND'
+        return (-1, 'ERR: object not found')
     elif num_found > 1:
-        print '    Object: TOO MANY OBJECTS FOUND'
+        return (-2, 'ERR: too many objects found')
 
 
 def find_direction(input):
@@ -44,15 +48,63 @@ def find_direction(input):
             num_found += 1
             direction = word
     if num_found == 1:
-        print ('    Direction: %s' % direction)
+        return (0, direction)
     elif num_found == 0:
-        print '    Direction: DIRECTION NOT FOUND'
+        return (-1, 'ERR: direction not found')
     elif num_found > 1:
-        print '    Direction: TOO MANY DIRECTIONS FOUND'
+        return (-2, 'ERR: too many directions found')
+
+
+def find_prep(input):
+    num_found = 0
+    words = input.split()
+    for word in words:
+        if word in prepositions:
+            num_found += 1
+            prep = word
+    if num_found == 1:
+        return (0, prep)
+    elif num_found == 0:
+        return (-1, 'ERR: direction not found')
+    elif num_found > 1:
+        return (-2, 'ERR: too many directions found')
 
 
 for user_input in input:
     print ('INPUT: %s' % user_input)
-    find_verb(user_input)
-    find_object(user_input)
-    find_direction(user_input)
+    verb = 'NULL'
+    obj = 'NULL'
+    prep = 'NULL'
+    direction = 'NULL'
+
+    (code, direction) = find_direction(user_input)
+    if code == 0:
+        print ("Moving %s" % direction)
+    else:
+        direction = 'NULL'
+
+    if direction == 'NULL':
+        (code, verb) = find_verb(user_input)
+        if code == -1:
+            print 'ERR: I can\'t tell what you\'re trying to do'
+            verb = 'NULL'
+        if code == -2:
+            print 'ERR: You\'re trying to do too many things at once'
+            verb = 'NULL'
+
+        (code, obj) = find_object(user_input)
+        if code == -1:
+            print 'ERR: I can\'t tell what you\'re trying to interact with'
+            obj = 'NULL'
+        if code == -2:
+            print 'ERR: You\'re trying to interact with too many things at once'
+            obj = 'NULL'
+
+        (code, prep) = find_prep(user_input)
+        if code == -1:
+            prep = 'NULL'
+        if code == -2:
+            prep = 'NULL'
+
+    command = [verb, obj, prep]
+    print command
