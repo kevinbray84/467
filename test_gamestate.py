a1cc68f56099495aec33fd5af415f5057e37cce7
hallyj <hallyj@oregonstate.edu>
2018-11-01 13:52:26 -0700

    testing look_at function

--- test_gamestate.py
+++ test_gamestate.py
@@ -3,12 +3,13 @@ from GameState import *
 from Input_Parser.input_parser import *
 
 def test_look_at():
+    print("\ntest 1: trying to look at mail while in First Floor Foyer \n")
     game = GameState()
     action = Input_Parser()
     action.obj = 'mail'
 
     game._look_at(action)
-    print("test 1 complete")
+    print("\ntest 1 complete")
 
 
 test_look_at()
