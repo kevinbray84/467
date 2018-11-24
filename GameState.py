3ce526c7eaa2fd2188c982fb28869855897d953f
hallyj <hallyj@oregonstate.edu>
2018-11-24 11:28:09 -0800

    fixed wine cellar not updating description bug

--- GameState.py
+++ GameState.py
@@ -811,7 +811,7 @@ class GameState:
                 if object_name in self.main_player.inventory:
                     self.winecellar_wall_unlocked = True
                     print_split(self.main_player.inventory['silver key'].use['correct'])
-                    self.current_room.is_locked == False # added to get around description print
+                    self.current_room.is_locked = False # added to get around description print
                     self.current_room.linked_rooms['east'] = self.json_Mansion["Secret Room"]
                     return self
                 else:
